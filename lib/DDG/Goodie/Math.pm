f89ba047427774308d20e98ffb32335ccaaf9b8e
Sam Whited <sam@samwhited.com>
2013-06-25 15:37:09 -0400

    Add MathJax locally

--- lib/DDG/Goodie/Math.pm
+++ lib/DDG/Goodie/Math.pm
@@ -13,16 +13,17 @@ zci answer_type => "math";
 
 attribution github => ['https://github.com/SamWhited', 'SamWhited'];
 primary_example_queries '$\sum_{i=0}^{N} x_i$';
-description 'Render maths using LaTeX or MathML via MathJax';
+description 'Render maths using LaTeX via MathJax';
 name 'math';
-code_url 'https://github.com/SamWhited/zeroclickinfo-goodie-math/blob/master/lib/DDG/Goodie/Math.pm';
+code_url 'https://github.com/duckduckgo/zeroclickinfo-goodie-math/blob/master/lib/DDG/Goodie/Math.pm';
 category 'computing_tools';
 topics 'math';
 
 
 handle query_raw => sub {
+    my $ffile = share('MathJax.js');
     my $html = '<script type="text/javascript"
-                src="https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+                src="'.$ffile.'?config=TeX-AMS-MML_HTMLorMML">
                 </script><script type="text/javascript">
                 MathJax.Hub.Config({
                   tex2jax: {
--- t/load.t
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env perl
-
-use strict;
-use warnings;
-use Test::More;
-
-use_ok('DDG::Goodie::Math');
-
-done_testing;
